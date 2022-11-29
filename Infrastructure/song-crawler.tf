2a274a65cfb2ff76db015dc58f0f98ab6eaf1df7
Aleksandra Maneva <maneva.aleksandra@gmail.com>
2022-11-29 17:23:58 +0100

    adding subnet

--- Infrastructure/song-crawler.tf
+++ Infrastructure/song-crawler.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "song_crawler" {
   function_name = "song_crawler"
   filename       = "build/song_crawler.zip"
-  role          = "arn:aws:iam::087020219185:role/EMR_EC2_DefaultRole"
+  role          = "arn:aws:iam::087020219185:role/LabRole"
   handler       = "crawler.handler"
   timeout       = 300
   runtime       = "python3.9"
--- vpc.tf
+++ vpc.tf
@@ -4,3 +4,13 @@ resource "aws_vpc" "myvpc" {
     Name = "My VPC"
   }
 }
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Public subnet"
+  }
+}
+
