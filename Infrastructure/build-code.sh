74124e5206a928ff0d3286ebdfc4e2702de98a94
Aleksandra Maneva <maneva.aleksandra@gmail.com>
2022-12-06 15:50:11 +0100

    deploy lambda and modified some files

--- Infrastructure/build-code.sh
+++
@@ -1,5 +0,0 @@
-mkdir Infrastructure/build
-
-cd Lambda-src
-zip -r ../Infrastructure/build/lambda-src.zip .
-cd ..
--- Infrastructure/song-crawler.tf
+++ Infrastructure/song-crawler.tf
@@ -1,10 +1,10 @@
 resource "aws_lambda_function" "song_crawler" {
   function_name = "song_crawler"
-  filename       = "build/song_crawler.zip"
+  filename      = "build/song-crawler.zip"
   role          = "arn:aws:iam::087020219185:role/LabRole"
   handler       = "crawler.handler"
   timeout       = 300
   runtime       = "python3.9"
-  source_code_hash = filebase64sha256("build/job_crawler.zip")
+  source_code_hash = filebase64sha256("build/song-crawler.zip")
 
 }
