a6ac09223445628e2bd255f6e38ddf01cba96684
Aleksandra Maneva <maneva.aleksandra@gmail.com>
2022-12-06 09:56:31 +0100

    changed line 14

--- api-server/src/main.py
+++ api-server/src/main.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://billboard-api2.p.rapidapi.com/hot-100"
 
-querystring = {"date":"1992-11-19","range":"1-1"}
+querystring = {"date":"2022-12-03","range":"1-3"}
 
 headers = {
 	"X-RapidAPI-Key": "dbd4ad8b76mshdddba42df9c1d1dp148e1fjsna693f9cc8ea5",
@@ -11,5 +11,5 @@ headers = {
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 
-print(response.text)
+print(response.json())
 
