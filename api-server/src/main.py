afbb9b44f8c9f90058f16b92ef51ac9cd0a2f19d
Aleksandra Maneva <maneva.aleksandra@gmail.com>
2022-12-07 13:41:06 +0100

    changed the api source

--- api-server/src/main.py
+++ api-server/src/main.py
@@ -1,15 +1,15 @@
 import requests
 
-url = "https://billboard-api2.p.rapidapi.com/hot-100"
+url = "https://netflix54.p.rapidapi.com/search/"
 
-querystring = {"date":"2022-12-03","range":"1-3"}
+querystring = {"query":"stranger","offset":"0","limit_titles":"50","limit_suggestions":"20"}
 
 headers = {
 	"X-RapidAPI-Key": "dbd4ad8b76mshdddba42df9c1d1dp148e1fjsna693f9cc8ea5",
-	"X-RapidAPI-Host": "billboard-api2.p.rapidapi.com"
+	"X-RapidAPI-Host": "netflix54.p.rapidapi.com"
 }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 
-print(response.json())
+print(response.text)
 
