71b2ae4ec3f67abeac8cf0f582633fde01ca9380
Aleksandra Maneva <maneva.aleksandra@gmail.com>
2022-12-07 16:08:20 +0100

    edited song_crawler and fixed pipeline

--- Infrastructure/song-crawler.tf
+++ Infrastructure/song-crawler.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "song_crawler" {
   function_name = "song_crawler"
   filename      = "build/song-crawler.zip"
   role          = "arn:aws:iam::087020219185:role/LabRole"
-  handler       = "crawler.handler"
+  handler       = "song_crawler.lambda_handler"
   timeout       = 300
   runtime       = "python3.9"
   source_code_hash = filebase64sha256("build/song-crawler.zip")
