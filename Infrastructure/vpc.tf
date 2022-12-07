39c72315bce2d6586e30b665ae1130f8a12303e8
Aleksandra Maneva <maneva.aleksandra@gmail.com>
2022-12-07 13:09:14 +0100

    Added security group

--- Infrastructure/vpc.tf
+++ Infrastructure/vpc.tf
@@ -43,3 +43,28 @@ resource "aws_route_table_association" "public_subnet_association" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow http"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description = "Http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
+}
