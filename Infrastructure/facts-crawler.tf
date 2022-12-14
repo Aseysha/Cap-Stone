544fa5dff153db90cda8852798339777a16d3ef0
Aleksandra Maneva <maneva.aleksandra@gmail.com>
2022-12-14 09:46:22 +0100

    added layers

--- Infrastructure/facts-crawler.tf
+++ Infrastructure/facts-crawler.tf
@@ -1,12 +1,12 @@
 resource "aws_lambda_function" "song_crawler" {
   function_name = "facts_crawler"
-  filename      = "build/facts-crawler.zip"
+  filename      = "build/curiousfacts1-crawler.zip"
   role          = "arn:aws:iam::087020219185:role/LabRole"
   handler       = "facts_crawler.lambda_handler"
   timeout       = 300
   runtime       = "python3.9"
   layers        = [aws_lambda_layer_version.lambda_layer.arn]
-  source_code_hash = filebase64sha256("build/facts-crawler.zip")
+  source_code_hash = filebase64sha256("build/curiousfacts1-crawler.zip")
 
 }
 
