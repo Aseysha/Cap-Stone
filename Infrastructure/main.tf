71e0cf9a443ce2ccb340007b072623088c795d3e
Aleksandra Maneva <maneva.aleksandra@gmail.com>
2022-12-15 14:06:27 +0100

    facts-crawler.py modified

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -11,15 +11,4 @@ provider "aws" {
   region = "us-west-2"
 }
 
-backend "s3" {
-    bucket = "terraform-state-12312d123fa"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
-  }
-
-provider "aws" {
-  region = "us-west-2"
-}
-
-
 data "aws_caller_identity" "current" {}
